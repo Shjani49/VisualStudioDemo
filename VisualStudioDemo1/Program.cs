899fadcc5167a2fcd63952c7ace89d0b3ad47d25
Shjani49 <shjani49@gmail.com>
2020-08-20 12:23:33 -0600

    Initial commit

---
+++ VisualStudioDemo1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisualStudioDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
