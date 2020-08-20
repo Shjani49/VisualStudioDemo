e84eca3a7a3f648b48e9fb3bcb9c1ddfa1fffaa8
Shjani49 <shjani49@gmail.com>
2020-08-20 15:44:52 -0600

    Create a  another class  and object .

--- VisualStudioDemo1/Program.cs
+++ VisualStudioDemo1/Program.cs
@@ -1,53 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace VisualStudioDemo1
+namespace VisualStudioDemoRedo
 {
     class Program
     {
-
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            int[,] theArray = new int[2, 2];
+            Person bob = new Person();
+            bob.Name = "Bob";
+            bob.Age = 50;
 
-            string userInput = "";
-
-            do
+            Person joe = new Person
             {
-                DisplayArray(theArray);
+                Name = "Joe",
+                Age = 25
+            };
 
-                Console.Write("Please enter \"Exit\" to quit, or anything else to continue: ");
-                userInput = Console.ReadLine();
+            Person defaultPerson = new Person();
 
-                if (userInput.ToLower() != "exit")
-                {
-                    int row, column, value;
+            Console.WriteLine(bob.Greeting());
+            Console.WriteLine(joe.Greeting());
+            Console.WriteLine(defaultPerson.Greeting());
 
-                    Console.Write("Please enter a row number: ");
-                    row = int.Parse(Console.ReadLine());
-
-                    Console.Write("Please enter a column number: ");
-                    column = int.Parse(Console.ReadLine());
-
-                    Console.Write("Please enter a new value: ");
-                    value = int.Parse(Console.ReadLine());
-
-                    // Modify the array
-                }
-            } while (userInput.ToLower() != "exit");
-        }
-
-        static void DisplayArray(int[,] arrayToDisplay)
-        {
-            Console.WriteLine("-----");
-            for (int r = 0; r < 2; r++)
-            {
-                Console.Write("|");
-                for (int c = 0; c < 2; c++)
-                {
-                    Console.Write(arrayToDisplay[c, r] + "|");
-                }
-                Console.WriteLine("\n-----");
-            }
         }
     }
-        }
+}
